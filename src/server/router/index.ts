3f9ebe1e48a83f8f7eb7c2a87b9133a49b99eaa7
SwimmingPotato <ptphong200i@gmail.com>
2022-10-11 16:27:10 +0700

    Removed example files

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,13 +1,11 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
 import superjson from "superjson";
-import { protectedExampleRouter } from "./protected-example-router";
 import { postRouter } from "./post";
 import { commentRouter } from "./comment";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("auth.", protectedExampleRouter)
   .merge("post.", postRouter)
   .merge("comment.", commentRouter);
 
